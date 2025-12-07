51fc9f2641c01bbd282e14af48dd88545908ba79
mahmoudkarim <k-arim25@hotmail.fr>
2025-12-08 00:13:34 +0100

    (update) docs :wrench:

--- docs/flutter_bootstrap.js
+++ docs/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"b5990e5ccc5e325fd24f0746e7d6689bbebc7c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2898829335"
+    serviceWorkerVersion: "1880194409"
   }
 });
